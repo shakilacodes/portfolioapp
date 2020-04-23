130975a973ef2941be8faa1f54bd941545872296
ShakilaCodes <shakilamuhammad@gmail.com>
2020-04-23 16:42:43 -0400

    Basic profile complete

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
+import illustration from "./assets/Illustration.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
+
+  render() {
+    return (
+      <div>
+        <img
+          src={illustration}
+          alt="profile illustration"
+          className="profile"
+        />
+        <h1>Hi!</h1>
+        <p>My name is Shakila</p>
+        <p>I'm always looking for new opportunities to grow.</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live and work in Philly</p>
+            <p>My favorite language is JavaScript</p>
+            <p>I'm learning to love React</p>
+            <button onClick={this.toggleDisplayBio}>Show less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )}
+        <hr />
+        <Projects />
+        <hr />
+        <SocialProfiles />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,30 @@
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: "Example React App",
+    description:
+      "A React App that I built, involving JS and core web dev concepts",
+    link: "https://github.com/shakilacodes?tab=repositories",
+    image: project2,
+  },
+  {
+    id: 2,
+    title: "Starships",
+    description: "A React App using the Star Wars API",
+    link: "https://github.com/shakilacodes/starshipfly",
+    image: project1,
+  },
+  {
+    id: 3,
+    title: "tbd",
+    description: "Placeholder for the next app that I build",
+    link: "https://github.com/shakilacodes?tab=repositories",
+    image: project3,
+  },
+];
+
+export default PROJECTS;
