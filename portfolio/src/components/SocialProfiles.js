c9bf9e11e7a944deff0d9a04e87c0a98fa73ef50
ShakilaCodes <shakilamuhammad@gmail.com>
2020-04-24 11:31:33 -0400

    Added stateless functions

--- portfolio/src/components/SocialProfiles.js
+++ portfolio/src/components/SocialProfiles.js
@@ -1,42 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import SOCIAL_PROFILES from "../data/socialProfiles";
 
-class SocialProfile extends Component {
-  render() {
-    const { link, image } = this.props.socialProfile;
+const SocialProfile = (props) => {
+  const { link, image } = props.socialProfile;
 
-    return (
-      <span>
-        <a href={link}>
-          <img
-            src={image}
-            alt="social profile"
-            style={{ width: 35, height: 35, margin: 10 }}
+  return (
+    <span>
+      <a href={link}>
+        <img
+          src={image}
+          alt="social profile"
+          style={{ width: 35, height: 35, margin: 10 }}
+        />
+      </a>
+    </span>
+  );
+};
+
+const SocialProfiles = () => (
+  <div>
+    <h2>Connect with me!</h2>
+    <div>
+      {SOCIAL_PROFILES.map((SOCIAL_PROFILE) => {
+        return (
+          <SocialProfile
+            key={SOCIAL_PROFILE.id}
+            socialProfile={SOCIAL_PROFILE}
           />
-        </a>
-      </span>
-    );
-  }
-}
-
-class SocialProfiles extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Connect with me!</h2>
-        <div>
-          {SOCIAL_PROFILES.map((SOCIAL_PROFILE) => {
-            return (
-              <SocialProfile
-                key={SOCIAL_PROFILE.id}
-                socialProfile={SOCIAL_PROFILE}
-              />
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
-}
+        );
+      })}
+    </div>
+  </div>
+);
 
 export default SocialProfiles;
