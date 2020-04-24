81cf14f41648736d232371ab467060d9f1272e8a
ShakilaCodes <shakilamuhammad@gmail.com>
2020-04-24 14:47:57 -0400

    Added Header

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -4,7 +4,6 @@ import Projects from "./Projects";
 import SocialProfiles from "./SocialProfiles";
 import illustration from "../assets/Illustration.png";
 import Title from "./Title";
-import Jokes from "./Jokes";
 
 class App extends Component {
   state = { displayBio: false };
@@ -41,8 +40,6 @@ class App extends Component {
         <Projects />
         <hr />
         <SocialProfiles />
-        <hr />
-        <Jokes />
       </div>
     );
   }
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
 import "./index.css";
 import App from "./components/App";
+import Jokes from "./components/Jokes";
 import * as serviceWorker from "./serviceWorker";
+import Header from "./components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={createBrowserHistory()}>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Header>
+              <App />
+            </Header>
+          )}
+        />
+        <Route
+          path="/jokes"
+          render={() => (
+            <Header>
+              <Jokes />
+            </Header>
+          )}
+        />
+      </Switch>
+    </Router>
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
